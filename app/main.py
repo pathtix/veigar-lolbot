cb669a6056b330f75b26227b83e47aef00d35103
pathtix <yayburcugenco@gmail.com>
2022-01-09 00:26:40 +0300

    html eklendi
    .

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app= Flask(__name__)
 
 @app.route('/')
 def index():
   return "<h1>Merhaba</h1>"
+
+@app.route("/home")
+def about():
+  return render_template("main.html")
